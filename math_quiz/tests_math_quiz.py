94eec450b181d1c0c30f7cddb25807c557594a41
Ramisa Bhuiyan <ramisa.bh24@gmail.com>
2024-11-09 23:01:15 +0100

    Refectored tests math quiz with improved readability

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,38 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_number, find_random_operator, create_problem_and_solution
 
 
-class TestMathGame(unittest.TestCase):
+class TestMathQuiz(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_number(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+        min_value = 1
+        max_value = 10
+        for _ in range(1000):  # Test multiple random values
+            random_value = generate_random_number(min_value, max_value)
+            self.assertTrue(min_value <= random_value <= max_value)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_find_random_operator(self):
+        # Test if the operator selected is one of the expected values '+', '-', '/', '%', '*'
+        valid_operators = ['+', '-', '/', '%', '*']
+        for _ in range(1000):
+            operator = find_random_operator()
+            self.assertIn(operator, valid_operators)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_create_problem_and_solution(self):
+        # Define test cases for different operators
+        test_cases = [
+            (3, 2, '+', '3 + 2', 5),
+            (5, 3, '-', '5 - 3', 2),
+            (3, 6, '*', '3 * 6', 18)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for value_1, value_2, operator, desired_problem, desired_answer in test_cases:
+            problem, answer = create_problem_and_solution(value_1, value_2, operator)
+            self.assertEqual(problem, desired_problem)
+            self.assertEqual(answer, desired_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
+
