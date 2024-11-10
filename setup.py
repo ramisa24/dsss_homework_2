cc3224fb171f63ee4d56eae06efad848348187f5
Ramisa Bhuiyan <ramisa.bh24@gmail.com>
2024-11-10 16:11:22 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "math_quiz",
+    version = "0.0.3",
+    author = "Ramisa Bhuiyan Raka",
+    license = "Apache 2.0",
+    long_description = long_description,
+    url = "https://github.com/ramisa24/dsss_homework_2.git"
+)
